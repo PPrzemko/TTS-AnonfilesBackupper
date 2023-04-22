6b9921c6b694a923b336c3d7c2bc63eb0fbcf124
TheWhiteSheep <thw.leon@gmail.com>
2023-04-22 20:50:45 +0200

    update database creation and file adding

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 import requests
 from dotenv import load_dotenv
 
+
 class FileInfo:
     def __init__(self, path, name, size):
         self.path = path
@@ -20,10 +21,9 @@ class FileInfo:
             id = int(tmpid)
         return id
 
-
 def get_files_in_directory():
     files = []
-    for entry in os.scandir('./'):
+    for entry in os.scandir(os.getenv('MOD_PATH')):
         if entry.is_file():
             file_name = entry.name
             file_path = os.path.join(entry.path, file_name)
@@ -32,39 +32,73 @@ def get_files_in_directory():
                 files.append(FileInfo(file_path, file_name, file_size))
     return files
 
+def create_database():
+    # Connect to the database
+    conn = sqlite3.connect('data.db')
+    if conn:
+        print("Database connection successful")
+    else:
+        print("Connection could not be established (big trouble)\n")
+        return
+    cursor = conn.cursor()
+    # Define the table to check for
+    table_name = 'files'
+    # Execute a query to check if the table exists
+    query = f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'"
+    cursor.execute(query)
 
+    # Check if the table exists in the database
+    if cursor.fetchone():
+        print('Use existing database')
+    else:
+        print('Table creation...')
+        cursor.execute('''
+                CREATE TABLE "files" (
+                "name"	TEXT,
+                "path"	TEXT,
+                "filesize"	TEXT,
+                "workshopid"	TEXT,
+                "anon_fileid"	TEXT,
+                "anon_link" TEXT,
+                "anon_success"	TEXT,
+                "anon_lastChecked"	TEXT,
+                UNIQUE("workshopid"),
+                PRIMARY KEY("workshopid"),
+                UNIQUE("anon_fileid")
+                     )''')
 
-def compare_to_database():
-    db_path = './data.db'
-    conn = sqlite3.connect(db_path)
-    c = conn.cursor()
-
-
-
-    # Insert data
-    #c.execute("INSERT INTO files (name, path, filesize, workshopid) VALUES (?, ?, ?, ?)",
-    #          ('test', 'John', 30, 54353453))
-
-    for file in files:
-        # Define the dataset to search for
-        dataset = (file.workshop_id,)
-
-        # Execute a SELECT query on the table that contains the dataset
-        query = 'SELECT * FROM files WHERE workshopid = ?'
-        c.execute(query, dataset)
-        if c.fetchone():
-            print('Dataset exists in database')
-        else:
-            print('Dataset does not exist in database')
-
-
-
-
-
-    # Commit changes
+    # Close and commit the database connection
     conn.commit()
     conn.close()
 
+def update_database(files):
+    conn = sqlite3.connect('data.db')
+    cursor = conn.cursor()
+    alreadyInDb = 0
+    newlyAdded = 0
+
+    for file in files:
+        workshopid = (file.workshop_id,)
+        # Check if the dataset is already in the database
+        select_query = "SELECT * FROM files WHERE workshopid=?"
+        cursor.execute(select_query, workshopid)
+
+        if cursor.fetchone() is None:
+            # If the dataset is not in the database, insert it
+            my_data = (file.name, file.path, file.filesize, file.workshop_id,)
+            insert_query = 'INSERT INTO files (name, path, filesize, workshopid) VALUES (?, ?, ?, ?)'
+            cursor.execute(insert_query, my_data)
+            conn.commit()
+            newlyAdded = newlyAdded + 1
+        else:
+            # If the dataset is already in the database, do nothing
+            # TODO: maybe implement version controll via filesize or hash
+            alreadyInDb = alreadyInDb + 1
+
+    print("Added " + str(newlyAdded) + " new Datasets")
+    print("Found " + str(alreadyInDb) + " old Datasets")
+    cursor.close()
+    conn.close()
 
 def curl_test():
     load_dotenv()
@@ -93,51 +127,62 @@ def curl_test():
         else:
             print('Error uploading file')
 
-
-def create_database():
-    # Connect to the database
+def upload_files(files):
+    load_dotenv()
+    url = "https://api.anonfiles.com/upload?token=" + os.getenv('API_KEY')
     conn = sqlite3.connect('data.db')
     cursor = conn.cursor()
-    # Define the table to check for
-    table_name = 'files'
-    # Execute a query to check if the table exists
-    query = f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'"
-    cursor.execute(query)
 
-    # Check if the table exists in the database
-    if cursor.fetchone():
-        print('Use existing database')
-    else:
-        print('Table creation...')
-        cursor.execute('''
-                CREATE TABLE "files" (
-                "name"	TEXT,
-                "path"	TEXT,
-                "filesize"	TEXT,
-                "workshopid"	TEXT,
-                "anon_fileid"	TEXT,
-                "anon_link" TEXT,
-                "anon_success"	TEXT,
-                "anon_lastChecked"	TEXT,
-                UNIQUE("workshopid"),
-                PRIMARY KEY("workshopid"),
-                UNIQUE("anon_fileid")
-                     )''')
+    for file in files:
+        for file in files:
+            workshopid = (file.workshop_id,)
+            # Check if the dataset is already in the database
+            select_query = "SELECT * FROM files WHERE workshopid=?"
+            cursor.execute(select_query, workshopid)
 
-    # Close the database connection
+            filedata = cursor.fetchone()
+            if filedata is None:
+                # If the dataset is not in the database, insert it
+                print("Error file is not in dataset")
+            else:
+                file_id = filedata[0]
+                file_name = filedata[1]
+                file_size = filedata[2]
+                print(f"File {file_name} with ID {file_id} and size {file_size} is already in the dataset")
+
+    cursor.close()
     conn.close()
 
 
+
 if __name__ == '__main__':
+    load_dotenv()
     create_database()
     files = get_files_in_directory()
-    compare_to_database()
-    for file in files:
-        print(file.name)
-        print(file.path)
-        print(file.filesize)
-        print(file.workshop_id)
-        print()
+    update_database(files)
 
-    curl_test()
+    menu = '4'
+
+    while menu != '0':
+        menu = input("0 - exit \n"
+                     "1 - upload newly added files \n"
+                     "2 - verify if the uploaded files are still available \n"
+                     "3 - export to excel/csv? \n"
+                     )
+        if menu == '1':
+            upload_files(files)
+        elif menu == '2':
+            print(2)
+        elif menu == '3':
+            print(3)
+
+    # compare_to_database()
+    # for file in files:
+    #    print(file.name)
+    #   print(file.path)
+    #  print(file.filesize)
+    # print(file.workshop_id)
+    # print()
+
+    # curl_test()
     input("Press any key to exit...")
